6e4314a7035f81f8c0a79119bc29abc9b658c846
Zaur <ztakata1998@gmail.com>
2019-02-27 12:03:40 +0300

    Delonghi demo

--- static/js/main.js
+++ static/js/main.js
@@ -1,27 +1,3 @@
 $(document).ready(function () {
-	$("#products").slick({
-	  infinite: false,
-	  dots: true,
-	  arrows: false,
-	  slidesToShow: 4,
-	  slidesToScroll: 4,
-	  autoplay: true,
-  	  autoplaySpeed: 2000,
-  	  responsive: [
-	    {
-	      breakpoint: 768,
-	      settings: {
-	        slidesToShow: 3,
-	        slidesToScroll: 3,
-	      }
-	    },
-	    {
-	      breakpoint: 575,
-	      settings: {
-	        slidesToShow: 2,
-	        slidesToScroll: 2,
-	      }
-	    },
-      ]
-	});
+
 })
