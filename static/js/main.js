acb95cfa6b70332de807baf48d08ab8c7aea942e
Zaur <ztakata1998@gmail.com>
2019-02-27 19:43:55 +0300

    delonghi home

--- static/js/main.js
+++ static/js/main.js
@@ -1,3 +1,54 @@
 $(document).ready(function () {
 
+	/* слайдер Рекомендованные товары */
+	function recommendedSwitch() {
+		let recItems = $('.recommended-list__item');
+		let recItemsImg = $('.recommended-list__img');
+		let count = recItems.length - 1;
+
+		let recShowItems = $('.recommended-show__item');
+		$(recShowItems).hide();
+		$(recShowItems[count]).show();
+		
+		if($(window).width() > "575") {
+		    $('.recommended-list__arrow').on('click', function () {
+				if (count < 1) {
+					count = recItems.length;
+				}
+				count--;
+				recItems.each(function(index) {
+				  	$(recItemsImg[index]).removeClass('recommended-list__item_act');
+				  	$(recShowItems[index]).hide();
+				});
+				$(recItemsImg[count]).addClass('recommended-list__item_act');
+				$(recShowItems[count]).show();
+			});
+		}
+		else {
+			recItems.each(function(index) {
+			  	$(recItems[index]).append( $(recShowItems[index]) );
+			});
+			$(recItems).on('click', function (event) {
+				recItems.each(function(index) {
+				  	$(recItemsImg[index]).removeClass('recommended-list__item_act');
+				  	$(recShowItems[index]).hide();
+				});
+
+				recItems.each(function(index) {
+				  	$(this).find(".recommended-show__info").hide();
+					$(this).find(".recommended-show__item").hide();
+				});
+					$(this).find(".recommended-list__img").addClass('recommended-list__item_act');
+				
+				$(this).find(".recommended-show__info").show();
+				$(this).find(".recommended-show__item").show();
+			});
+		}
+	}
+
+	recommendedSwitch();
+
+
+
 })
+
