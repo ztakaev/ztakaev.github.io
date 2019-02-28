2ddf41a99d44a5f98ea1cfeb726f7302beacb72b
Zaur <ztakata1998@gmail.com>
2019-02-28 09:18:26 +0300

    wgh

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
 	/* слайдер Рекомендованные товары */
+	/*
 	function recommendedSwitch() {
 		let recItems = $('.recommended-list__item');
 		let recItemsImg = $('.recommended-list__img');
@@ -45,10 +46,56 @@ $(document).ready(function () {
 			});
 		}
 	}
+	*/
+	
+
+	function recommendedSwitch() {
+		let recItems = $('.recommended-list__item');
+		let recItemsImg = $('.recommended-list__img');
+		let count = recItems.length - 1;
+
+		let recShowItems = $('.recommended-show__item');
+		$(recShowItems).hide();
+		$(recShowItems[count]).show();
+		
+		if($(window).width() > "575") {
+		    $(recItems).on('click mouseenter', function (event) {
+				let index = $(this).index();
+
+		    	recShowItems.each(function(index) {
+				  	$(recShowItems[index]).hide();
+					$(recShowItems[index]).hide();
+				});
+
+		    	recItems.each(function(index) {
+				  	$(recItemsImg[index]).removeClass('recommended-list__item_act');
+				});
+				$(this).find(".recommended-list__img").addClass('recommended-list__item_act');
+				$(recShowItems[index]).show();
+		    });	    
+		}
+		else {
+			recItems.each(function(index) {
+			  	$(recItems[index]).append( $(recShowItems[index]) );
+			});
+			$(recItems).on('click', function (event) {
+				recItems.each(function(index) {
+				  	$(recItemsImg[index]).removeClass('recommended-list__item_act');
+				  	$(recShowItems[index]).hide();
+				});
+
+				recItems.each(function(index) {
+				  	$(this).find(".recommended-show__info").hide();
+					$(this).find(".recommended-show__item").hide();
+				});
+					$(this).find(".recommended-list__img").addClass('recommended-list__item_act');
+				
+				$(this).find(".recommended-show__info").show();
+				$(this).find(".recommended-show__item").show();
+			});
+		}
+	}
 
 	recommendedSwitch();
-
-
-
 })
 
